d4be95fec39ccaef6af6be41586f96ce4642bef0
Pham Tu <tubackkhoa@gmail.com>
2023-05-18 13:49:09 +0700

    update code

--- packages/contracts-build/src/index.ts
+++ packages/contracts-build/src/index.ts
@@ -1,16 +1,19 @@
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
+import { MarginedEngineTypes, MarginedFeePoolTypes, MarginedInsuranceFundTypes, MarginedPricefeedTypes, MarginedVammTypes } from '@oraichain/oraimargin-contracts-sdk';
 import { readFileSync } from 'fs';
 import path from 'path';
 
-const contractDir = path.join(path.dirname(module.filename), '..', 'data');
-
 export type ContractName = 'margined_vamm' | 'margined_fee_pool' | 'margined_pricefeed' | 'margined_insurance_fund' | 'margined_engine';
+export type InstantiateMsg = MarginedEngineTypes.InstantiateMsg | MarginedFeePoolTypes.InstantiateMsg | MarginedPricefeedTypes.InstantiateMsg | MarginedInsuranceFundTypes.InstantiateMsg | MarginedVammTypes.InstantiateMsg;
+export type MigrateMsg = MarginedInsuranceFundTypes.MigrateMsg;
+
+const contractDir = path.join(path.dirname(module.filename), '..', 'data');
 
 export const getContractDir = (name: ContractName = 'margined_engine') => {
   return path.join(contractDir, name + '.wasm');
 };
 
-export const deployContract = async <T>(client: SigningCosmWasmClient, senderAddress: string, msg: T, label: string, contractName?: ContractName) => {
+export const deployContract = async (client: SigningCosmWasmClient, senderAddress: string, msg: InstantiateMsg, label: string, contractName?: ContractName) => {
   // upload and instantiate the contract
   const wasmBytecode = readFileSync(getContractDir(contractName));
   const uploadRes = await client.upload(senderAddress, wasmBytecode, 'auto');
@@ -18,7 +21,7 @@ export const deployContract = async <T>(client: SigningCosmWasmClient, senderAdd
   return { ...uploadRes, ...initRes };
 };
 
-export const migrateContract = async <T>(client: SigningCosmWasmClient, senderAddress: string, contractAddress: string, msg: T, contractName?: ContractName) => {
+export const migrateContract = async (client: SigningCosmWasmClient, senderAddress: string, contractAddress: string, msg: MigrateMsg, contractName?: ContractName) => {
   // upload and instantiate the contract
   const wasmBytecode = readFileSync(getContractDir(contractName));
   const uploadRes = await client.upload(senderAddress, wasmBytecode, 'auto');
