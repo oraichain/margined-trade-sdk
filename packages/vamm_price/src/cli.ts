f0c99d8d629ee192fab7060aaffa9b6874a8fbca
Hieu Le <levanhieuhust@gmail.com>
2023-09-21 08:13:29 +0700

    add discord noti for vamm price bot

--- packages/vamm_price/src/cli.ts
+++ packages/vamm_price/src/cli.ts
@@ -2,25 +2,43 @@ import dotenv from "dotenv";
 import { queryAllVammSpotPrice } from "./index";
 import { decrypt, delay, setupWallet } from "@oraichain/oraimargin-common";
 import WebSocket from "ws";
+import { WebhookClient, time, blockQuote, userMention } from "discord.js";
+
 dotenv.config();
 
 const wss = new WebSocket.Server({ port: 3001 });
 
-const sendPrice = async (time: number, prices: string[]) => {
+const sendPrice = async (timeStamp: number, prices: string[], webhookClient: WebhookClient) => {
   for (const spotPrice of prices) {
     wss.clients.forEach((ws) => {
       ws.send(
         JSON.stringify({
           event: "market_price",
           pair_price: spotPrice,
-          time,
+          time: timeStamp,
         })
       );
     });
+
+    await webhookClient.send(
+      `💵 ${spotPrice} ` +
+      `at ${time(new Date())}`
+    );
   }
 };
 
 (async () => {
+  const webhookUrl = process.env.DISCORD_WEBHOOK ?? "";
+  console.log({ webhookUrl });
+
+  if (webhookUrl === "") {
+    console.log("Discord webhook is not set!");
+  }
+
+  const webhookClient = new WebhookClient({
+    url: webhookUrl,
+  });
+
   const insurance = process.env.INSURANCE_FUND_CONTRACT;
   const sendTime = process.env.SEND_TIME ? Number(process.env.SEND_TIME) : 3600;
   console.log({ sendTime });
@@ -42,7 +60,6 @@ const sendPrice = async (time: number, prices: string[]) => {
     clientConnected = true;
   });
 
-  // TODO: send logs to discord
   while (true) {
     try {
       let curTime = Math.floor(Date.now() / 1000);
@@ -58,18 +75,18 @@ const sendPrice = async (time: number, prices: string[]) => {
       if (differencePrices.length > 0) {
         console.log("SEND CHANGED PRICE");
         console.log({ differencePrices });
-        sendPrice(curTime, differencePrices);
+        sendPrice(curTime, differencePrices, webhookClient);
       }
 
       if (clientConnected) {
         console.log("CLIENT CONNECTED - SEND PRICES");
         clientConnected = false;
-        sendPrice(curTime, alLPrices);
+        sendPrice(curTime, alLPrices, webhookClient);
       }
       if (curTime - preTime >= sendTime) {
         console.log("SEND PRICES SEQUENTIALLY");
         preTime = curTime;
-        sendPrice(curTime, alLPrices);
+        sendPrice(curTime, alLPrices, webhookClient);
       }
     } catch (error) {
       console.error("Error updating oracle price: ", error);
